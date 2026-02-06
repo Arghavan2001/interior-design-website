d9a3fe6d01b70376cfd5c5ade6a77ca6a0c8c4c0
Arghavan Amiri <arghavanamiri2001@gmail.com>
2026-02-06 19:28:12 +0330

    add countUp

--- docs/script.js
+++ docs/script.js
@@ -392,3 +392,23 @@ if(menu_bar){
             },1000);
         })
     }
+
+
+
+//CounterUp
+
+function toPersianNumber(number) {
+    const persianDigits = ['۰','۱','۲','۳','۴','۵','۶','۷','۸','۹'];
+    return number.toString().replace(/\d/g, d => persianDigits[d]);
+  }
+
+const counter1 = new countUp.CountUp('year', 15, { duration: 3, formattingFn: toPersianNumber });
+if (!counter1.error) counter1.start();
+
+// شمارنده دوم
+const counter2 = new countUp.CountUp('satisfication', 3200, { duration: 3, formattingFn: toPersianNumber });
+if (!counter2.error) counter2.start();
+
+// شمارنده سوم
+const counter3 = new countUp.CountUp('projects', 780, { duration: 2, formattingFn: toPersianNumber });
+if (!counter3.error) counter3.start();
